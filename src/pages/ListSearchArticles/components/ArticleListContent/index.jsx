57170b8dd82dc07cd5a9f99b163b8cf77a3e5c80
wdj1993 <wangdingjia@aliyun.com>
2020-07-02 22:59:31 +0800

    圈子图片展示

--- src/pages/ListSearchArticles/components/ArticleListContent/index.jsx
+++ src/pages/ListSearchArticles/components/ArticleListContent/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 import styles from './index.less';
 
-const ArticleListContent = ({ data: { content } }) => (
+const ArticleListContent = ({ data: { content, pics } }) => (
   <div className={styles.listContent}>
     <div className={styles.description}>{content}</div>
+    <div className={styles.pics}>
+    <Row gutter={[10,10]}>
+      {pics.map((pic) => (
+        <Col span={8} key={pic.o_img}>
+          <div className={styles.listCircleImg}>
+            <img src={pic.o_img} alt="说说图片" object-fit="contain" />
+          </div>
+        </Col>
+      ))}
+    </Row>
+    </div>
   </div>
 );
 
